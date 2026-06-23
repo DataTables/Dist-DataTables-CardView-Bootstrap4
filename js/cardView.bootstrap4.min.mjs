e70b1409b5c11a6218be5fe41d414ad6f36c3df3
Allan Jardine <allan@datatables.net>
2026-06-23 15:20:58 +0000

    Initial commit

---
+++ js/cardView.bootstrap4.min.mjs
@@ -0,0 +1,4 @@
+/*! CardView Bootstrap 4 styling 0.0.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
+ */
+import DataTable from"datatables.net-bs4";import CardView from"datatables.net-cardview";Object.assign(DataTable.CardView.classes,{card:"card dtcv-card",cardContent:"card-body"});export default DataTable;
